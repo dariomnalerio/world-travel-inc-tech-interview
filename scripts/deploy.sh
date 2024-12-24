a8c3b586a71cb146302125884fa8eb7d8c6c63b7
dariomnalerio <dariomnalerio@gmail.com>
2024-12-24 19:47:29 -0300

    chore: update docker-compose configuration & deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,6 +6,10 @@ DOCKERHUB_TOKEN=$3
 EC2_HOST=$4
 SSH_USERNAME=$5
 
+mkdir -p ~/.ssh
+
+ssh-keyscan -H $EC2_HOST >> ~/.ssh/known_hosts
+
 echo "StrictHostKeyChecking no" >> ~/.ssh/config
 
 scp docker-compose.yml $SSH_USERNAME@$EC2_HOST:~/docker-compose.yml
@@ -14,6 +18,6 @@ ssh $SSH_USERNAME@$EC2_HOST "
   echo $DOCKERHUB_TOKEN | docker login --username $DOCKERHUB_USERNAME --password-stdin
   docker-compose pull
   docker-compose down
-  docker-compose up -d
+  docker-compose --profile prod up -d
   docker image prune -f
 "
