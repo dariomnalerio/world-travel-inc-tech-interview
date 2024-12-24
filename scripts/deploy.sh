c8e0f56a8a482feaf74fadfc275f5dcbfabf1956
dariomnalerio <dariomnalerio@gmail.com>
2024-12-24 20:43:17 -0300

    chore: add install_docker.sh script and update deploy.sh to automate Docker installation

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -13,8 +13,11 @@ ssh-keyscan -H $EC2_HOST >> ~/.ssh/known_hosts
 echo "StrictHostKeyChecking no" >> ~/.ssh/config
 
 scp docker-compose.yml $SSH_USERNAME@$EC2_HOST:~/docker-compose.yml
+scp scripts/install_docker.sh $SSH_USERNAME@$EC2_HOST:~/install_docker.sh
 
 ssh $SSH_USERNAME@$EC2_HOST "
+  chmod +x ~/install_docker.sh
+  ~/install_docker.sh $SSH_USERNAME
   echo $DOCKERHUB_TOKEN | docker login --username $DOCKERHUB_USERNAME --password-stdin
   docker-compose pull
   docker-compose down
---
+++ scripts/install_docker.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Check if the username is provided as an argument
+if [ -z "$1" ]; then
+  echo "Usage: $0 <username>"
+  exit 1
+fi
+
+USERNAME=$1
+
+sudo yum update -y
+yum install docker
+sudo service docker start
+sudo chkconfig docker on
+sudo usermod -a -G docker $USERNAME
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+sudo reboot
+
+docker --version
+docker-compose --version
+
