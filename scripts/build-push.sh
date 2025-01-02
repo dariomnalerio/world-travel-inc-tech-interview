9401174bb3b68865ced0e6bb280d05b0836be6b4
dariomnalerio <dariomnalerio@gmail.com>
2025-01-02 17:35:03 -0300

    feat(docker): upgrade PostgreSQL version to 17 in all docker-compose files and update build scripts for production Dockerfiles

--- scripts/build-push.sh
+++ scripts/build-push.sh
@@ -4,13 +4,13 @@ GITHUB_SHA=$1
 DOCKERHUB_USERNAME=$2
 
 # Build and push client
-docker build -t $DOCKERHUB_USERNAME/client:$GITHUB_SHA ./client
+docker build -t $DOCKERHUB_USERNAME/client:$GITHUB_SHA -f ./client/Dockerfile.prod ./client
 docker push $DOCKERHUB_USERNAME/client:$GITHUB_SHA
 docker tag $DOCKERHUB_USERNAME/client:$GITHUB_SHA $DOCKERHUB_USERNAME/client:latest
 docker push $DOCKERHUB_USERNAME/client:latest
 
 # Build and push server
-docker build -t $DOCKERHUB_USERNAME/server:$GITHUB_SHA ./server
+docker build -t $DOCKERHUB_USERNAME/server:$GITHUB_SHA -f ./server/Dockerfile.prod ./server
 docker push $DOCKERHUB_USERNAME/server:$GITHUB_SHA
 docker tag $DOCKERHUB_USERNAME/server:$GITHUB_SHA $DOCKERHUB_USERNAME/server:latest
 docker push $DOCKERHUB_USERNAME/server:latest
