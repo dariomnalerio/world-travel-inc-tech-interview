fb00fd25ca688aa2b5d3fcf770c39826af85f982
dariomnalerio <dariomnalerio@gmail.com>
2025-01-06 13:38:01 -0300

    fix(client): simplify API_BASE_URL definition by removing environment dependency

--- client/src/api/index.ts
+++ client/src/api/index.ts
@@ -1,3 +1,1 @@
-const env = import.meta.env.MODE
-const API_URL = import.meta.env.VITE_API_URL
-export const API_BASE_URL = env !== "production" ? "/api" : API_URL
+export const API_BASE_URL = "/api"
